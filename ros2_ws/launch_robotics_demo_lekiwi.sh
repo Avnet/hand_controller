a8f3611af4074fa9ff1544e7545032769b46376f
AlbertaBeef <grouby177@gmail.com>
2025-12-29 23:53:59 +0000

    ros2_ws: rename original launch script as *_mogiros.sh, and add new *_lekiwi.sh launch script.

--- ros2_ws/launch_robotics_demo_lekiwi.sh
+++ ros2_ws/launch_robotics_demo_lekiwi.sh
@@ -6,6 +6,6 @@ export WAYLAND_DISPLAY=wayland-1
 export HOME=/home
 export ROS_DOMAIN_ID=0
 source /usr/bin/ros_setup.sh && source /usr/share/qirp-setup.sh
-source /root/hand_controller/ros2_sw/install/setup.bash
-ros2 launch hand_controller demo11_mogiros_car_part1_ei1dials.launch.py verbose:=False model:=/root/hand_controller/hands-v2-yolov5-linux-aarch64-qnn-v36.eim use_flask:=False use_imshow:=False x_t:=0.20 z_t:=0.20 | ros2 run hand_controller gtk_gui_node
+source /root/hand_controller/ros2_ws/install/setup.bash
+ros2 launch hand_controller demo13_lekiwi_part1_ei1dials.launch.py verbose:=False model:=/root/hand_controller/hands-v2-yolov5-linux-aarch64-qnn-v36.eim use_flask:=False use_imshow:=False x_t:=0.20 z_t:=0.20 | ros2 run hand_controller gtk_gui_node
 
