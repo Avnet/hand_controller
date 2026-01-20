33c9634d3cbbdf8180db019d350886e8a5595245
AlbertaBeef <grouby177@gmail.com>
2026-01-20 02:31:06 +0000

    ros2_ws: lulower intensity of angular.z commands.

--- ros2_ws/launch_robotics_demo_lekiwi_qairt_asl.sh
+++ ros2_ws/launch_robotics_demo_lekiwi_qairt_asl.sh
@@ -14,5 +14,5 @@ source /root/qairt/2.40.0.251030/bin/envsetup.sh
 export ADSP_LIBRARY_PATH=$QNN_SDK_ROOT/lib/hexagon-v${DSP_ARCH}/unsigned
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$QNN_SDK_ROOT/lib/aarch64-oe-linux-gcc11.2
 
-ros2 launch hand_controller demo13_lekiwi_part1_qai2asl.launch.py verbose:=False use_flask:=False use_imshow:=False x_t:=0.20 x_a:=0.0 x_b:=10.0 z_t:=0.20 z_a:=0.0 z_b:=2.0 threshold_detector_minscore:=0.6 threshold_landmark_confidence:=0.6 | ros2 run hand_controller gtk_gui_node
+ros2 launch hand_controller demo13_lekiwi_part1_qai2asl.launch.py verbose:=False use_flask:=False use_imshow:=False x_t:=0.20 x_a:=0.0 x_b:=10.0 z_t:=0.20 z_a:=0.0 z_b:=1.0 threshold_detector_minscore:=0.6 threshold_landmark_confidence:=0.6 | ros2 run hand_controller gtk_gui_node
 
